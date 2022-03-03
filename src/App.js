eef7ab5ffd16d36c93d8a2353832f05b0f7c01ed
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2022-03-03 16:19:21 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm/>
+      {/* <PostList/> */}
+    </div>
+  );
+}
+
+export default App;
